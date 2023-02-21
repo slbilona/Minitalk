604397a69ea5ed85f67e97c7e96268eb3cebbd05
Ilona SELBONNE <ilselbon@e1r1p8.clusters.42paris.fr>
2023-02-21 19:53:56 +0100

    21/02 19h54

--- client.c
+++ client.c
@@ -1,8 +1,67 @@
-int main(int ac, char **av)
+#include <sys/types.h>
+#include <signal.h>
+#include <stdio.h>
+
+int	ft_atoi(const char *nptr)
 {
-    if(ac == 3)
-    {
-        
-    }
+	int	result;
+	int	i;
+	int	sign;
+
+	sign = 1;
+	i = 0;
+	result = 0;
+	while ((9 <= nptr[i] && nptr[i] <= 13) || nptr[i] == ' ')
+		i++;
+	if (nptr[i] == '-' || nptr[i] == '+')
+	{
+		if (nptr[i] == '-')
+			sign = sign * -1;
+		i++;
+	}
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		result = result * 10 + nptr[i] - 48;
+		i++;
+	}
+	return (result * sign);
+}
+
+void ft_binaire(int c)
+{
+	int i;
+	char result[30];
+	char *base = "01";
+
+	i = 0;
+	if(c >= 0 && c <= 1)
+	{
+		result[i] = base[c];
+		i++;
+	}
+	else if (c > 1)
+	{
+		ft_binaire(c / 2);
+		ft_binaire(c % 2);
+	}
+	printf("%s\n", result);
+}
+
+
+int main()
+{
+	// int pid;
+
+	// if(ac == 3)
+	// {
+	// 	pid = ft_atoi(av[1]);
+	// 	if(pid == -1)
+	// 	{
+	// 		printf("error\n");
+	// 		return (0);
+	// 	}
+	// 	kill(pid, SIGUSR1);
+	// }
+	ft_binaire('s');
 }
 
--- server.c
+++ server.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <signal.h>
+
+void ft_franchement_jsp()
+{
+	printf("envoye\n");
+}
+
+void ft_peut_etre_que_je_sais()
+{
+	printf("encore\n");
+}
 
 int main()
 {
-    int pid_t = getpid();
-    printf("PID : %d\n", pid_t);
-    
-    return (0);
+	int pid_t = getpid();
+	printf("PID : %d\n", pid_t);
+	signal(SIGUSR1, ft_franchement_jsp);
+	pause();
+	return (0);
 }
